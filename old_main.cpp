34a591628f004cb9c383cb5c61f17f7dd93346d6
jesuslovesyoutoday <73426135+jesuslovesyoutoday@users.noreply.github.com>
2021-02-11 19:22:04 +0300

    profiler was rewritten for class 'subvector'
    function 'unit_test' was added to 'subvector' as a class method

--- old_main.cpp
+++ old_main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
-#include "old_subvector.cpp"
+#include "subvector.cpp"
 
 int main()
 {
     subvector vec;
-    init(&vec);
-    push_back(&vec, 1);   
-    push_back(&vec, 2);   
-    push_back(&vec, 3);   
-
-    print_subvector(&vec);
-
-    resize(&vec, 10);
-
-    shrink_to_fit(&vec);
-
-    std::cout << pop_back(&vec) << std::endl;
-    std::cout << pop_back(&vec) << std::endl;
-    std::cout << pop_back(&vec) << std::endl;
-
-    clear(&vec);
-
-    destructor(&vec);
+    vec.unit_test();
+    
     return 0;
 }
